60f2a53d61131cfba8660144d00b52a7649078b6
behemothaur <duncan.rutland@gmail.com>
2015-12-28 16:23:29 -0600

    Initial refactoring

--- aws-recon.rb
+++ aws-recon.rb
@@ -3,6 +3,96 @@ require 'aws-sdk-core'
 require 'trollop'
 require 'oj'
 
+class AWSService
+  def initialize(args={})
+    @metadata     = args[:metadata]
+    @clients      = args[:clients]
+
+    @attributes       = @metadata[:attributes]
+    @collection       = @metadata[:collection]
+    @describe_method  = @metadata[:describe_method]
+    @client           = @clients[@metadata[:client]] 
+    @describe_output      = @client.send(@describe_method) 
+    @totals           = Hash.new
+  end
+
+  def fetch
+    @describe_output = @client.send(@describe_method)
+  end
+
+  def has_items?
+    @describe_output[@collection].any?
+  end
+
+  def set_title(args={})
+    status  = args[:status]    
+    table   = args[:table]
+
+    puts "DEBUG: #{@attributes}"
+
+    if status
+      table.title = "= SUMMARY for: " + @metadata[:name]
+    else
+      table.title = "= NO DATA for: " + @metadata[:name]
+    end
+  end
+
+  def set_column_labels(args={})
+    table   = args[:table]
+    @attributes.each {|attribute| table.label.push(attribute[:column_name])}
+  end
+
+  def set_totals(args={})
+    table  = args[:table]  
+
+    @attributes.each do |attribute|
+      if attribute[:count]
+        table.sum.push(@totals[:count])
+      elsif @totals[attribute[:target]]
+        table.sum.push(@totals[attribute[:target]])
+      else
+        table.sum.push("N/A")
+      end
+    end
+  end
+end
+
+class Table
+  attr_accessor :title
+
+  def initialize(args={})
+    @col    = 0
+    @row    = 0
+    @title  = "Empty table"
+    @header = Array.new
+    @sum    = Array.new
+    @matrix = Array.new
+  end
+
+  def next_col
+    @col += 1
+  end
+
+  def next_row
+    @row += 1
+  end
+
+  def push(args={})
+    @matrix[@row][@col] = args[:data]
+  end
+
+  def pop
+    @matrix[@row][@col]
+  end
+
+  def render
+    puts @header.join(",") 
+    @matrix.each {|item| puts item.join(",")}
+    puts @sum.join(",")
+  end
+end
+
+
 # Parse options
 opts = Trollop::options do
   version "aws-recon v0.1 (c) 2015 Duncan Rutland"
@@ -35,75 +125,30 @@ Aws.config.update({credentials: creds})
 
 # Populate hash with AWS SDK client objects
 clients   = { ec2: Aws::EC2::Client.new,
-              elb: Aws::ElasticLoadBalancing::Client.new,
-              rds: Aws::RDS::Client.new,
-              elasticache: Aws::ElastiCache::Client.new
-}
+                  elb: Aws::ElasticLoadBalancing::Client.new,
+                  rds: Aws::RDS::Client.new,
+                  elasticache: Aws::ElastiCache::Client.new }
 
 # Load service definitions
 metadata  = Oj.load_file("services.json")
 
 # Function definitions
 
-def print_section_header(args={})
-  title   = args[:title]
-  status  = args[:status]    
-  printf "\n"
-  if status
-    printf "= SUMMARY for: %-20s\n",title
-  else
-    printf "= NO DATA for: %-20s\n",title
-  end
-  printf "==============\n"
-end
-
-def print_column_labels(args={})
-  name        = args[:name]
-  attributes  = args[:attributes]
-  printf "\n"
-  attributes.each {|attribute| printf "%-#{attribute[:width]}s", attribute[:column]}
-  printf "\n"
-  attributes.each {|attribute| printf "%-#{attribute[:width]}s", "-" * attribute[:column].size}
-  printf "\n"
-end
-
-def print_totals(args={})
-  attributes  = args[:attributes]
-  totals      = args[:totals]
-
-  attributes.each {|attribute| printf "%-#{attribute[:width]}s", "-" * attribute[:column].size}
-
-  printf "\n"
-
-  attributes.each do |attribute|
-    if attribute[:count]
-      printf "%-#{attribute[:width]}s",totals[:count]
-    elsif totals[attribute[:target]]
-      printf "%-#{attribute[:width]}s",totals[attribute[:target]] 
-    else
-      printf "%-#{attribute[:width]}s","N/A"
-    end
-  end
-  printf "\n"
-  attributes.each {|attribute| printf "%-#{attribute[:width]}s","-" * attribute[:column].size}
-  printf "\n"
-end
-
-def print_data(args={})
+def parse_data(args={})
   data        = args[:data]
-  attributes  = args[:attributes]
-
-  sum = {}
-  sum[:count] = 0
+  table       = args[:table]
+  service     = args[:service]
 
   data.each do |datum|
     i         = 0 
     continue  = true
+
+    # Iterate row by row until no more data to display
     while continue
       continue = false
-      attributes.each do |attribute|
+      service.attributes.each do |attribute|
         case attribute[:type]
-        when :unary
+        when :scalar
           if i.zero?
             if attribute[:stub]
               output = datum[attribute[:stub]][attribute[:target]]
@@ -119,59 +164,49 @@ def print_data(args={})
              output = tag.key + " = " + tag.value
              continue = true if i < datum[attribute[:stub]].size - 1
           end
-        when :list  
-          item = datum[attribute[:collection]][i]
-          if item
-            if attribute[:stub]
-              output = item[attribute[:stub]][attribute[:target]]
-            else
-              output = item[attribute[:target]]
-            end
-            continue = true if i < datum[attribute[:collection]].size - 1
-          end
-        else
+        when :array
+          parse_data({data: datum, table: table, service: service})
         end
-        printf "%-#{attribute[:width]}s", output.to_s.slice(0,attribute[:width]-1)
+
+        table.push(output.to_s)
 
         if attribute[:sum]
-          sum[attribute[:target]] = 0 if sum[attribute[:target]].nil?
-          sum[attribute[:target]] += output.to_i
+          service.sum[attribute[:target]] = 0 if service.sum[attribute[:target]].nil?
+          service.sum[attribute[:target]] += output.to_i
         end
+        table.next_col
       end
       i += 1
       puts "\n"
     end
-    sum[:count] += 1
+    service.sum[:count] += 1
+    table.next_row
   end
-  return sum
 end
 
 # Main loop
-metadata[:services].each do |service|
-  section_name    = service[:name] 
-  attributes      = service[:attributes]
-  collection      = service[:collection]
-  client          = clients[service[:client]]
-  description     = client.send(service[:describe_method])
+metadata[:services].each do |item|
+  puts "Starting main loop for #{item}"
+  service = AWSService.new({ metadata: item, clients: clients })
+  output  = Table.new
 
-  if description[collection].any?
-    print_section_header({status: true, title: section_name})
-    print_column_labels({name: section_name, attributes: attributes})
+  if service.has_items?
+    service.set_title({status: true, table: output})
+    print_column_labels({name: service.name, attributes: service.attributes})
 
     total_rows  = 0
-    totals      = {}
 
-    if section_name == "EC2"
-      description.reservations.each do |reservation|
-        totals = print_data({data: reservation.instances, attributes: attributes}) 
-        total_rows += totals[:count]
+    if service.name == "EC2"
+      service.describe_output.reservations.each do |reservation|
+        parse_data({ data: reservation.instances, service: service, table: output }) 
       end
-      totals[:count] = total_rows
     else
-      totals = print_data({data: description[collection], attributes: attributes}) 
+      parse_data({data: describe_output[collection], service: service, table: output}) 
     end
-    print_totals({attributes: attributes, totals: totals })
+    service.set_totals({table: output})
+    
+    output.render
   else
-    print_section_header({status: false, title: section_name})
+    service.set_title({status: false, table: output})
   end
 end
