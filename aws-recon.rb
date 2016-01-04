2054427647c1dd28b8706f327fd469a6561d6aae
behemothaur <duncan.rutland@gmail.com>
2016-01-04 11:22:54 -0600

    Refactored with classes for AWSService and Table + recursive parsing

--- aws-recon.rb
+++ aws-recon.rb
@@ -4,31 +4,37 @@ require 'trollop'
 require 'oj'
 
 class AWSService
-  def initialize(args={})
-    @metadata     = args[:metadata]
-    @clients      = args[:clients]
+  attr_accessor :name, :attributes, :describe_output, :totals
 
+  def initialize(args={})
+    @metadata         = args[:metadata]
+    @clients          = args[:clients]
+
+    @name             = @metadata[:name]
     @attributes       = @metadata[:attributes]
-    @collection       = @metadata[:collection]
+    @collection       = @metadata[:collection_name]
     @describe_method  = @metadata[:describe_method]
     @client           = @clients[@metadata[:client]] 
-    @describe_output      = @client.send(@describe_method) 
-    @totals           = Hash.new
+    @describe_output  = @client.send(@describe_method) 
+    @totals           = { count: 0 }
   end
 
-  def fetch
+  def fetch_collection
     @describe_output = @client.send(@describe_method)
+    return @describe_output[@collection]
   end
 
   def has_items?
     @describe_output[@collection].any?
   end
 
+  def num_items
+    @describe_output[@collection].size
+  end
+
   def set_title(args={})
     status  = args[:status]    
     table   = args[:table]
-
-    puts "DEBUG: #{@attributes}"
 
     if status
       table.title = "= SUMMARY for: " + @metadata[:name]
@@ -39,7 +45,7 @@ class AWSService
 
   def set_column_labels(args={})
     table   = args[:table]
-    @attributes.each {|attribute| table.label.push(attribute[:column_name])}
+    @attributes.each {|attribute| table.header.push(attribute[:column_name])}
   end
 
   def set_totals(args={})
@@ -47,26 +53,30 @@ class AWSService
 
     @attributes.each do |attribute|
       if attribute[:count]
-        table.sum.push(@totals[:count])
+        table.footer.push(@totals[:count])
       elsif @totals[attribute[:target]]
-        table.sum.push(@totals[attribute[:target]])
+        table.footer.push(@totals[attribute[:target]])
       else
-        table.sum.push("N/A")
+        table.footer.push("N/A")
       end
     end
   end
 end
 
 class Table
-  attr_accessor :title
+  attr_accessor :title, :header, :footer
+  MAX_ROWS = 128
 
   def initialize(args={})
+    @num_rows = args[:rows] || MAX_ROWS
     @col    = 0
     @row    = 0
+    @head   = 0
+    @tail   = 0
     @title  = "Empty table"
     @header = Array.new
-    @sum    = Array.new
-    @matrix = Array.new
+    @footer = Array.new
+    @matrix = Array.new(@num_rows) { Array.new }
   end
 
   def next_col
@@ -75,9 +85,21 @@ class Table
 
   def next_row
     @row += 1
+    @tail = [@row,@tail].max
+  end
+
+  def new_row
+    @head = @tail + 1
+    @row  = @head
+    @col  = 0
+  end
+
+  def reset_row
+    @row = @head
   end
 
   def push(args={})
+    puts "DEBUG [Table.push]: data = #{args[:data]}, row = #{@row}, col = #{@col}"
     @matrix[@row][@col] = args[:data]
   end
 
@@ -86,9 +108,11 @@ class Table
   end
 
   def render
+    puts @title
     puts @header.join(",") 
-    @matrix.each {|item| puts item.join(",")}
-    puts @sum.join(",")
+    @matrix.each {|item| puts item.join(",") if !item.empty?}
+    puts @footer.join(",")
+    puts "\n"
   end
 end
 
@@ -101,6 +125,7 @@ opts = Trollop::options do
   opt :profile, "Specify AWS credential profile name", short: "-p", type: String
   opt :role, "ARN for role to assume", short: "-R", type: String
   opt :extid, "External ID for STS Assume Role", short: "-x", type: String
+  opt :debug, "Enable debugging output", short: "-D", default: false, type: :boolean 
   depends :role, :extid
   conflicts :profile, :role
   conflicts :profile, :extid
@@ -124,89 +149,109 @@ end
 Aws.config.update({credentials: creds})
 
 # Populate hash with AWS SDK client objects
-clients   = { ec2: Aws::EC2::Client.new,
-                  elb: Aws::ElasticLoadBalancing::Client.new,
-                  rds: Aws::RDS::Client.new,
-                  elasticache: Aws::ElastiCache::Client.new }
+clients   = { ec2:          Aws::EC2::Client.new,
+              elb:          Aws::ElasticLoadBalancing::Client.new,
+              rds:          Aws::RDS::Client.new,
+              elasticache:  Aws::ElastiCache::Client.new }
 
 # Load service definitions
 metadata  = Oj.load_file("services.json")
 
 # Function definitions
 
-def parse_data(args={})
-  data        = args[:data]
+def parse_datum(args={})
+  datum       = args[:datum]
   table       = args[:table]
   service     = args[:service]
+  attributes  = args[:attributes]
+  output      = nil
 
-  data.each do |datum|
-    i         = 0 
-    continue  = true
+  puts "+ DEBUG [parse_datum]: Parsing datum #{datum}"
+  puts "+ DEBUG [parse_datum]: Parsing #{attributes.size} attributes from #{attributes}"
 
-    # Iterate row by row until no more data to display
-    while continue
-      continue = false
-      service.attributes.each do |attribute|
-        case attribute[:type]
-        when :scalar
-          if i.zero?
-            if attribute[:stub]
-              output = datum[attribute[:stub]][attribute[:target]]
-            else
-              output = datum[attribute[:target]]
-            end
-          else
-            output = ""
-          end
-        when :tags  
-          tag = datum[attribute[:stub]][i]
-          if tag
-             output = tag.key + " = " + tag.value
-             continue = true if i < datum[attribute[:stub]].size - 1
-          end
-        when :array
-          parse_data({data: datum, table: table, service: service})
-        end
+  attributes.each do |attribute|
+    puts "++ DEBUG [parse_datum]: Parsing attribute #{attribute}"
 
-        table.push(output.to_s)
+    table.reset_row if attribute[:root_node]
 
-        if attribute[:sum]
-          service.sum[attribute[:target]] = 0 if service.sum[attribute[:target]].nil?
-          service.sum[attribute[:target]] += output.to_i
-        end
-        table.next_col
+    case attribute[:type]
+    when :scalar
+      if attribute[:stub]
+        output = datum[attribute[:stub]][attribute[:target]]
+      else
+        output = datum[attribute[:target]]
       end
-      i += 1
-      puts "\n"
+      table.push(data: output.to_s) if output
+    when :tags  
+      tags = datum[attribute[:stub]]
+      tags.each do |tag|
+        output = tag.key + " = " + tag.value
+        table.push(data: output) if output
+        table.next_row
+      end
+    when :collection
+      puts "+++ DEBUG [parse_datum]: Parsing #{datum[attribute[:collection_name]].size} items from array: #{datum[attribute[:collection_name]]}"
+      datum[attribute[:collection_name]].each do |item|
+        puts "++++ DEBUG [parse_datum]: Parsing item #{item}"
+        parse_datum({ datum:      item,
+                      attributes: attribute[:attributes],
+                      table:      table,
+                      service:    service})
+
+        if attribute[:leaf_node] && datum[attribute[:collection_name]].size > 0
+          puts "++++ DEBUG [parse_datum]: Current attribute is a leaf node... Incrementing row"
+          table.next_row
+        end
+      end
     end
-    service.sum[:count] += 1
-    table.next_row
+
+    if attribute[:sum]
+      service.totals[attribute[:target]] = 0 if service.totals[attribute[:target]].nil?
+      service.totals[attribute[:target]] += output.to_i
+    end
+  table.next_col
   end
 end
 
-# Main loop
+# Main loop - iterate through AWS services as defined in services.json
 metadata[:services].each do |item|
-  puts "Starting main loop for #{item}"
-  service = AWSService.new({ metadata: item, clients: clients })
-  output  = Table.new
 
-  if service.has_items?
-    service.set_title({status: true, table: output})
-    print_column_labels({name: service.name, attributes: service.attributes})
+  # Create a new AWSService object to contain metadata and client object
+  service = AWSService.new({ metadata:  item,
+                             clients:   clients })
 
-    total_rows  = 0
+  output  = Table.new                                       # Fresh table object to build output
+
+  if service.has_items?                                     # First check if the service has any items to process
+    service.set_column_labels(table: output)                # Populate column labels into the table object
+    service.set_title({status: true, table: output})        # Set the title for table
 
     if service.name == "EC2"
       service.describe_output.reservations.each do |reservation|
-        parse_data({ data: reservation.instances, service: service, table: output }) 
+        reservation.instances.each do |item|
+          parse_datum({ datum:      item,
+                        attributes: service.attributes,
+                        service:    service,
+                        table:      output }) 
+
+          service.totals[:count] += 1
+          output.new_row
+        end
       end
     else
-      parse_data({data: describe_output[collection], service: service, table: output}) 
+      service.fetch_collection.each do |item|
+         parse_datum({ datum:      item,
+                       attributes: service.attributes,
+                       service:    service,
+                       table:      output }) 
+
+         service.totals[:count] += 1
+         output.new_row
+      end
     end
     service.set_totals({table: output})
-    
-    output.render
   else
     service.set_title({status: false, table: output})
   end
+  output.render
 end
